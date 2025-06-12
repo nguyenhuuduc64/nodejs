f10bdd86171fcb68d5fe6ea9c3f3a3cf82dbe3ec
nguyenhuuduc64 <duchuuiq@gmail.com>
2025-06-12 21:37:53 +0700

    select all

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -1,22 +1,31 @@
-const Course = require('../models/Course');
-const { mongooseToObject, mutipleMongooseToObject } = require('../../util/mongoose');
+const Course = require("../models/Course");
+const {
+  mongooseToObject,
+  mutipleMongooseToObject,
+} = require("../../util/mongoose");
 class MeController {
-    storedCourses(req, res, next) {
-        Course.find({deleted: false}).then((courses) => {
-            res.render('me/storedCourses', {
-                courses: mutipleMongooseToObject(courses),
-            });
+  storedCourses(req, res, next) {
+    Promise.all([
+      Course.findDeleted({}), // ✅ sửa chỗ này
+      Course.find({ deleted: false }),
+    ])
+      .then(([deletedCourses, courses]) => {
+        res.render("me/storedCourses", {
+          deletedCount: deletedCourses.length,
+          courses: mutipleMongooseToObject(courses),
         });
-    }
-    trashCourses(req, res, next) {
-      Course.findDeleted().then(courses => {
-        const onlyDeleted = courses.filter(course => course.deleted);
-        res.render('me/trashCourses', { courses: mutipleMongooseToObject(onlyDeleted) });
-      });
-    }
-      
- 
+      })
+      .catch(next);
+  }
 
+  trashCourses(req, res, next) {
+    Course.findDeleted().then((courses) => {
+      const onlyDeleted = courses.filter((course) => course.deleted);
+      res.render("me/trashCourses", {
+        courses: mutipleMongooseToObject(onlyDeleted),
+      });
+    });
+  }
 }
 
 module.exports = new MeController();
